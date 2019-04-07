6d7c7e0ddc1c47d4d109b2ef05be82ea1bdfb7a0
dmitriybodlev <dmitriybodlev@gmail.com>
2019-04-07 18:12:49 +0300

    master/created youtube list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as R from 'ramda';
+import { Box, Flex, GlobalStyle } from './ui'
+import React, { useState, useEffect } from 'react';
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// Helpers
+export const getVideoTitle = (item) => `${R.add(item.position, 1)}. ${item.title}`;
+export const getVideoIdByPath = R.path(['resourceId', 'videoId'])
+// Helpers
+
+export const VideoContent = ({ item }) => (
+  <Flex alignItems='center' flexDirection='column' justifyContent='center'>
+    <Box p='0 20px' mb='20px' fontSize='24px'>{getVideoTitle(item)}</Box>
+    <Box height='50vw' maxWidth='640px' maxHeight='360px' width='calc(100vw - 40px)'>
+      <iframe
+        width='100%'
+        height='100%'
+        frameBorder='0'
+        type='text/html'
+        title={item.title}
+        src={`http://www.youtube.com/embed/${getVideoIdByPath(item)}?autoplay=0`} />
+    </Box>
+  </Flex>
+);
+
+export const ListItem = (props) => (
+  <Box m='0 5px' cursor='pointer' onClick={() => props.setActiveVideo(getVideoIdByPath(props.item))}>
+    <img alt={props.item.title} title={getVideoTitle(props.item)} src={R.path(['thumbnails', 'default', 'url'], props.item)} />
+  </Box>
+);
+
+const playlistId = 'PLPxbbTqCLbGE5AihOSExAa4wUM-P42EIJ';
+const apiKey = 'AIzaSyDBOi4s_dNXJ8KHR_7z-8QNL27e0iDtWW8';
+
+const App = () => {
+  const [ items, setItems ] = useState({})
+  const [ activeVideo, setActiveVideo ] = useState(null)
+  useEffect(() => {
+    if (R.not(R.isEmpty(items))) return;
+    fetch(
+      `https://www.googleapis.com/youtube/v3/playlistItems?key=${apiKey}&part=snippet&playlistId=${playlistId}`,
+      { method: 'GET' },
+    )
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      const items = R.map(R.prop('snippet'), data.items);
+      const activeVideo = getVideoIdByPath(R.head(items))
+      const indexedItems = R.indexBy(getVideoIdByPath, items);
+      setItems(indexedItems);
+      setActiveVideo(activeVideo);
+    })
+  })
+  return (
+    <div className='App'>
+      <GlobalStyle />
+      <Flex flexDirection='column' alignItems='center' justifyContent='center' width='100vw' height='100vh'>
+          {R.not(R.isNil(activeVideo)) && <VideoContent item={items[activeVideo]} />}
+          <Box p='5px 5px 0' mt='20px' border='1px solid lightgray' borderRadius='5px'>
+            <Flex maxWidth='650px' width='calc(100vw - 40px)' overflow='auto' height='max-content'>
+              {R.map((item) => (
+                <ListItem item={item} setActiveVideo={setActiveVideo} key={getVideoIdByPath(item)} />
+              ), R.values(items))}
+            </Flex>
+          </Box>
+      </Flex>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ui/index.js
@@ -0,0 +1,90 @@
+import * as R from 'ramda';
+import styled, { createGlobalStyle } from 'styled-components';
+import {
+  size,
+  color,
+  space,
+  width,
+  height,
+  border,
+  display,
+  fontSize,
+  flexWrap,
+  overflow,
+  maxWidth,
+  minWidth,
+  maxHeight,
+  minHeight,
+  alignItems,
+  borderColor,
+  borderRadius,
+  flexDirection,
+  justifyContent } from 'styled-system';
+// //////////////////////////////////////////////////////////////////////////////
+
+export const GlobalStyle = createGlobalStyle`
+  body, ul, button {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+  h1, h2, h3, h4, h5, h6, p {
+    margin: 0;
+    font-weight: 400;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * :active, :hover, :focus {
+    outline: 0;
+    outline-offset: 0;
+  }
+  *::-webkit-scrollbar-track
+  {
+    background-color: transparent;
+  }
+
+  *::-webkit-scrollbar
+  {
+    width: 8px;
+    height: 8px;
+  }
+
+  *::-webkit-scrollbar-thumb
+  {
+    background-color: lightgray;
+  }
+`
+
+export const Box = styled.div`
+  ${size}
+  ${color}
+  ${space}
+  ${width}
+  ${height}
+  ${border}
+  ${display}
+  ${fontSize}
+  ${overflow}
+  ${maxWidth}
+  ${minWidth}
+  ${maxHeight}
+  ${minHeight}
+  ${borderColor}
+  ${borderRadius}
+  cursor: ${({ cursor }) => R.or(cursor, 'initial')};
+`;
+
+export const Flex = styled(Box)`
+  ${flexWrap}
+  ${alignItems}
+  ${flexDirection}
+  ${justifyContent}
+  display: flex;
+`;
+
